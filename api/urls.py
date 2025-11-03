63e6864ef2b9ee70bbbf4015896b3f1daa4b16fc
Tanvir Rahman <mdtanvirt@gmail.com>
2025-11-03 13:30:06 +0600

    Update viewsets

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path
 from . import views
+from rest_framework.routers import DefaultRouter
 
-urlpatterns = [
-    path('products/', views.product_list),
-    path('product/<int:pk>', views.products_details),
+'''urlpatterns = [
+    #path('products/', views.product_list.as_view()),
+    #path('product/<int:pk>', views.products_details.as_view()),
     path('orders/', views.orders_list),
-]
+]'''
+
+router = DefaultRouter()
+router.register('product', views.product_list, basename='product')
+router.register('order', views.orders_list, basename='order')
+
+urlpatterns = router.urls
--- api/views.py
+++ api/views.py
@@ -4,20 +4,18 @@ from api.models import Product, Order, OrderItem
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
-@api_view(['GET'])
-def product_list(request):
-    products = Product.objects.all()
-    serilizer = ProductSerilizer(products, many=True)
-    return Response(serilizer.data)
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
-@api_view(['GET'])
-def products_details(request, pk):
-    product = get_object_or_404(Product, pk=pk)
-    serilizer = ProductSerilizer(product)
-    return Response(serilizer.data)
+from rest_framework import generics
 
-@api_view(['GET'])
-def orders_list(request):
-    orders = Order.objects.all()
-    serilizer = OrderSerilizer(orders, many=True)
-    return Response(serilizer.data)
+from rest_framework import viewsets
+
+class product_list(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerilizer
+
+class orders_list(viewsets.ModelViewSet):
+    queryset = Order.objects.all().prefetch_related('items__product')
+    serializer_class = OrderSerilizer
